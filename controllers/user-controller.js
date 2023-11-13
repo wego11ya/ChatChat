4331fd930a8128c7567a7eec7a1dff914f63f005
wego11ya <wego11ya@gmail.com>
2023-11-13 15:17:14 +0800

    feat: user signUp

---
+++ controllers/user-controller.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcryptjs");
+const { User } = require("../models");
+const userController = {
+  signUpPage: (req, res) => {
+    res.render("signup");
+  },
+  signUp: async (req, res, next) => {
+    try {
+      const { account, name, email, password, passwordConfirmation } = req.body;
+      if (
+        !account?.trim() ||
+        !name?.trim() ||
+        !email?.trim() ||
+        !password?.trim() ||
+        !passwordConfirmation?.trim()
+      )
+        throw new Error("All fields are required.");
+      if (password !== passwordConfirmation)
+        throw new Error("Password and confirmation password do not match.");
+      const [userByEmail, userByAccount] = await Promise.all([
+        User.findOne({ where: { email } }),
+        User.findOne({ where: { account } }),
+      ]);
+      if (userByEmail) throw new Error("Email is already registered");
+      if (userByAccount) throw new Error("Account is already registered.");
+      const user = await User.create({
+        account,
+        name,
+        email,
+        password: bcrypt.hashSync(password, 10),
+      });
+      res.redirect("/signin");
+    } catch (error) {
+      next(error);
+    }
+  },
+  testPage: (req, res) => {
+    res.render("test");
+  },
+};
+module.exports = userController;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 const homeController = require("../controllers/home-controller");
+const userController = require("../controllers/user-controller");
+router.get("/signup", userController.signUpPage);
+router.post("/signup", userController.signUp);
+router.get("/test", userController.testPage);
 router.get("/", homeController.getHomePage);
 module.exports = router;
