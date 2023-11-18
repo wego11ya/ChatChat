2936be6f01574402ca3e0e749adb4951182104b6
wego11ya <wego11ya@gmail.com>
2023-11-18 16:23:37 +0800

    feat: implement basic public-chatroom functionality

--- controllers/socket-controller.js
+++ controllers/socket-controller.js
@@ -1,1 +1,13 @@
-module.exports = (io) => {};
+module.exports = (io) => {
+  io.on("connection", function (socket) {
+    console.log("A user connected");
+    // 接收來自client端的訊息
+    socket.on("new message", function (msg) {
+      io.emit("new message", msg); // 廣播消息給所有連接的用戶
+    });
+
+    socket.on("disconnect", function () {
+      console.log("User disconnected");
+    });
+  });
+};
---
+++ public/js/chatroom.js
@@ -0,0 +1,22 @@
+// client-side initialization for socket.io
+const socket = io();
+
+// 監聽表單，當表單送出時，取得輸入的訊息，並透過socket.emit()傳送給server端
+document
+  .getElementById("message-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const message = document.getElementById("message-input").value;
+    if (message) {
+      socket.emit("new message", message);
+      document.getElementById("message-input").value = "";
+    }
+  });
+
+// 監聽server端傳來的訊息，並顯示在網頁上
+socket.on("new message", function (msg) {
+  const item = document.createElement("li");
+  item.textContent = msg;
+  document.getElementById("messages").appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
