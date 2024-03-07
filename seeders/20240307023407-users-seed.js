2a1c969e00cad5c365b517e737d5473c3489e5c1
wego11ya <wego11ya@gmail.com>
2024-03-07 17:03:35 +0800

    feat: add users seed

---
+++ seeders/20240307023407-users-seed.js
@@ -0,0 +1,26 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+const { faker } = require("@faker-js/faker");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Users",
+      Array.from({ length: 10 }, (_, index) => {
+        return {
+          name: faker.person.firstName(),
+          email: `user${index + 1}@example.com`,
+          password: bcrypt.hashSync("12345678", 10),
+          avatar: faker.image.avatar(),
+          introduction: faker.lorem.paragraph(2),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
