6aaafb3c9d06d7f09930040230da72ce5ecb3294
wego11ya <wego11ya@gmail.com>
2024-03-08 11:55:46 +0800

    feat update public-chats seeder for 20 messages from random users

--- seeders/20240307132325-public-chats-seed.js
+++ seeders/20240307132325-public-chats-seed.js
@@ -3,11 +3,24 @@ const { faker } = require("@faker-js/faker");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const usersIdArray = await queryInterface.sequelize.query(
+    const users = await queryInterface.sequelize.query(
       `SELECT id FROM Users;`,
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     );
-  },
 
-  async down(queryInterface, Sequelize) {},
+    const messages = [];
+    for (let i = 0; i < 20; i++) {
+      const user_id = users[Math.floor(Math.random() * users.length)].id;
+      messages.push({
+        user_id,
+        message: faker.lorem.sentence(),
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+    await queryInterface.bulkInsert("Public_Messages", messages, {});
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Public_Messages", null, {});
+  },
 };
